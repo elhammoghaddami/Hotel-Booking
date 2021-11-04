e7571f5dd1ec46c37c31953b4b47bc36f010a03a
e.moghadami <moghadami@rayanmehr.co.ir>
2021-11-04 18:59:36 +0330

    Add interface to core module

---
+++ src/app/core/interfaces/reserveObjGroup.interface.ts
@@ -0,0 +1,10 @@
+export interface ReserveObj {
+  hotelName: string;
+  startDate: string;
+  endDate: string;
+  adults: number;
+  child: number;
+  price_rate:number;
+  price:number;
+  dayCount:number
+}
